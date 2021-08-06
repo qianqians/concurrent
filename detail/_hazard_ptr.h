07bb1b781f94511cf3164195bf46e7f47daa376f
qianqians <theDarkforce@aliyun.com>
2021-08-06 10:45:08 +0800

    bug fixed:wrong notes

--- detail/_hazard_ptr.h
+++ detail/_hazard_ptr.h
@@ -22,7 +22,7 @@ namespace detail{
 template <typename X>
 struct _hazard_ptr{
 	X * _hazard; //
-	std::atomic_int32_t _active; // 0 unactive/1 active
+	std::atomic_int32_t _active; // 0 active/1 unactive
 };
 
 // hazard system 
@@ -39,7 +39,7 @@ private:
 		recover_list() : active(1) {re_vector.reserve(32);}
 
 		std::vector<_deallocate_data, _Allocator_deallocate_data> re_vector;
-		std::atomic_int32_t active; // 0 unactive/1 active
+		std::atomic_int32_t active; // 0 active/1 unactive
 	};
 
 	// allocator 
