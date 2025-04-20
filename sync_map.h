066dcf07ecc98301bd46378a1ec95eb95d4d1762
吴铭 <451517996@qq.com>
2025-04-20 23:44:41 +0800

    sync_map

--- sync_map.h
+++ sync_map.h
@@ -21,9 +21,14 @@ namespace concurrent {
 template <typename K, typename V>
 class sync_map {
 private:
+	template <typename V>
+	struct entry {
+		std::atomic<V*> p;
+	};
+
 	struct read_only {
-		std::shared_ptr<std::map<K, V>> m;
-		bool amended;
+		std::shared_ptr<std::map<K, std::shared_ptr<entry<V>>>> m = std::make_shared<std::map<K, std::shared_ptr<entry<V>>>>();
+		bool amended = false;
 	};
 
 	typedef concurrent::detail::_hazard_ptr<read_only> _hazard_ptr;
@@ -32,28 +37,42 @@ private:
 public:
 	sync_map() : _hazard_sys(std::bind(&sync_map::put_read_only, this, std::placeholders::_1)) {
 		_read.store(get_read_only());
-		_dirty = std::make_shared<std::map<K, V>>();
+		_dirty = std::make_shared<std::map<K, std::shared_ptr<entry<V>>>>();
 		misses = 0;
+	}
+
+	virtual ~sync_map() {
+		read_only* _p = _read.load();
+
+		if (_dirty) {
+			for (auto [k, e] : *(_dirty)) {
+				delete e->p.load();
+			}
+		}
+		else {
+			for (auto [k, e] : *(_p->m)) {
+				delete e->p.load();
+			}
+		}
+
+		if (_p) {
+			put_read_only(_p);
+		}
 	}
 
 private:
 	read_only* get_read_only(){
-		read_only* __r = new read_only();
-
-		__r->m = std::make_shared<std::map<K, V>>();
-		__r->amended = false;
-
-		return __r;
+		return new read_only();
 	}
 
 	void put_read_only(read_only* _p){
-		_hazard_sys.retire(_p);
+		delete _p;
 	}
 
 private:
 	std::mutex _mu;
 	std::atomic<read_only*> _read;
-	std::shared_ptr<std::map<K, V>> _dirty;
+	std::shared_ptr<std::map<K, std::shared_ptr<entry<V>>>> _dirty;
 	int misses;
 
 	_hazard_system _hazard_sys;
